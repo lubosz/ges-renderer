26e9c7f1584fd46a2a288c9be674a936d644dc1a
Lubosz Sarnecki <lubosz@gmail.com>
2013-10-29 13:09:10 +0100

    clean up memory in minimal demo

--- ges-minimal.c
+++ ges-minimal.c
@@ -42,42 +42,46 @@ main (int argc, char **argv)
   GESTimeline *timeline;
   GESLayer *layer;
   GESPipeline *pipeline;
+  GESTrack *track;
+  GMainLoop *mainloop;
+  GstBus *bus;
+  GESAsset *asset;
 
   gst_init (&argc, &argv);
   ges_init ();
 
-  timeline = ges_timeline_new_audio_video ();
-
-  GESTrack *track;
-
   timeline = ges_timeline_new ();
-
   track = GES_TRACK (ges_video_track_new ());
-
   ges_timeline_add_track (timeline, track);
 
   layer = ges_layer_new ();
   ges_timeline_add_layer (timeline, layer);
 
-  GESAsset *src_asset = ges_asset_request (GES_TYPE_TEST_CLIP, NULL, NULL);
-  ges_layer_add_asset (layer, src_asset, 0, 0, GST_SECOND, GES_TRACK_TYPE_UNKNOWN);
+  asset = ges_asset_request (GES_TYPE_TEST_CLIP, NULL, NULL);
+  ges_layer_add_asset (layer, asset, 0, 0, GST_SECOND, GES_TRACK_TYPE_VIDEO);
+  gst_object_unref (asset);
 
   ges_timeline_commit (timeline);
   
   pipeline = ges_pipeline_new ();
   ges_pipeline_add_timeline (pipeline, timeline);
 
-  GMainLoop *mainloop;
   mainloop = g_main_loop_new (NULL, FALSE);
 
-  GstBus *bus;
   bus = gst_pipeline_get_bus (GST_PIPELINE (pipeline));
   g_signal_connect (bus, "message", (GCallback) busMessageCb, mainloop);
   gst_bus_add_signal_watch (bus);
+  gst_object_unref (bus);
 
   gst_element_set_state (GST_ELEMENT (pipeline), GST_STATE_PLAYING);
 
   g_main_loop_run (mainloop);
+  gst_element_set_state (GST_ELEMENT (pipeline), GST_STATE_PAUSED);
+
+  gst_object_unref (track);
+  gst_object_unref (layer);
+  gst_object_unref (timeline);
+  gst_object_unref (pipeline);
   g_main_loop_unref (mainloop);
 
   return 0;
